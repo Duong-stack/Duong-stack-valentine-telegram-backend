a61604119c094d7d876e3d09fef5cd5361a74fe7
Duong-stack <nguyendinhhaianh102007@gmail.com>
2026-04-02 02:07:38 +0700

    Refactor index.js to improve code structure

--- index.js
+++ index.js
@@ -5,17 +5,14 @@ const fetch = require('node-fetch');
 const app = express();
 app.use(bodyParser.json({limit:'10mb'})); // cho ảnh base64 lớn
 
-// Token và chat ID Telegram
 const TELEGRAM_TOKEN = '8687534107:AAGBGRR74rfjYGeoV2erjk01piWdMCePMps';
 const CHAT_ID = '8202147321';
 
-// Route nhận ảnh từ frontend
 app.post('/upload', async (req, res) => {
   try {
     const { image } = req.body;
     if (!image) return res.status(400).send('No image');
 
-    // Gửi ảnh lên Telegram
     await fetch(`https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendPhoto`, {
       method:'POST',
       headers:{'Content-Type':'application/json'},
